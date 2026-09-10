156f45c83d63cc6c68f6779befc113f40d83c598
mtcummings21 <mtcummings21@gmail.com>
2026-09-10 13:12:27 -0500

    Add week 1 picks for player 'Z'
    Updated player 'Z' to include picks for week 1.

--- assets/data.js
+++ assets/data.js
@@ -15873,7 +15873,7 @@ const SURVIVOR = {
     "champion": null,
     "weekRevealTimes": { "1": "2026-09-13T12:00:00-04:00" },
     "players": [
-      { "name": "Z", "result": "Active", "eliminatedWeek": null, "picks": [] },
+      { "name": "Z", "result": "Active", "eliminatedWeek": null, "picks": [ { "week": 1, "team": "Chargers", "loss": false } ] },
       { "name": "Cummings", "result": "Active", "eliminatedWeek": null, "picks": [ { "week": 1, "team": "Chargers", "loss": false } ] },
       { "name": "Luke", "result": "Active", "eliminatedWeek": null, "picks": [] },
       { "name": "Aaron", "result": "Active", "eliminatedWeek": null, "picks": [ { "week": 1, "team": "Jaguars", "loss": false } ] },
